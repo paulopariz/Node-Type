e05254e5035fac1f6ea2f317793128aab9cbdb8b
paulopariz <paulopariz01@gmail.com>
2024-01-07 18:41:03 -0300

    feat - add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("sfd");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
